44958a2b8b098c7b8ad21af745342fdd7c6e7a09
agustinnielsen <agustinnielsen31@gmail.com>
2026-05-27 17:51:06 -0300

    Update and rename clasificador(1).py to clasificador.py

--- clasificador.py
+++ clasificador.py
@@ -5,15 +5,17 @@ import time
 import os
 from tqdm import tqdm
 
-# 1. CONFIGURACIÓN
-client = Groq(api_key="")
+# CONFIGURACIÓN
+client = Groq(api_key="") # Completar API personal desde https://console.groq.com/keys . Esto puede ser reemplazado por el LLM de esu conveniencia.
 MODEL_ID = "llama-3.1-8b-instant"
 ARCHIVO_IN = "oraciones_para_clasificar.csv"
 ARCHIVO_OUT = "analisis_valencia_final.csv"
 
-# 2. FILTRO BASURA
+# LIMPIEZA
 BASURA = ['newsletter', 'suscribite', 'seguinos en', 'clarín', 'la nación', 'infobae', 'leer más', 'registrate']
 
+# Definimos prompt base según el caso.
+## Se da contexto al modelo en búsqueda de refinar el resultado, se otorgan las categorías posibles y se pide un score de confianza.
 PROMPT_SISTEMA = """
 Actuá como clasificador de datos JSON experto en Sociología Computacional y Tecnologías Digitales. Tu tarea es analizar la valencia de oraciones sobre tecnología.
 Categorías: "positiva", "negativa", "no se identifica valencia".
@@ -23,7 +25,7 @@ REGLAS:
 JSON format: {"resultados": [{"id": 0, "clase": "...", "score": 0.0}]}
 """
 
-
+# Definimos función clasificadora.
 def clasificar_con_groq(bloque_oraciones):
     texto_input = "\n".join([f"ID {i}: {txt}" for i, txt in enumerate(bloque_oraciones)])
     try:
@@ -40,16 +42,16 @@ def clasificar_con_groq(bloque_oraciones):
     except Exception:
         return "REINTENTAR"
 
-
+# Definimos función de ejecución.
 def ejecutar():
     df = pd.read_csv(ARCHIVO_IN, sep=';', encoding='utf-8-sig')
 
-    # Lógica de reanudación
+    # Lógica de reanudación en caso de interrupción
     if os.path.exists(ARCHIVO_OUT):
         df_ya_hecho = pd.read_csv(ARCHIVO_OUT, sep=';')
         ultimo_indice = len(df_ya_hecho)
         resultados = df_ya_hecho.to_dict('records')
-        print(f"🔄 Reanudando desde la oración {ultimo_indice}...")
+        print(f"Reanudando desde la oración {ultimo_indice}...")
     else:
         resultados = []
         ultimo_indice = 0
@@ -62,7 +64,7 @@ def ejecutar():
         res_bloque = clasificar_con_groq(batch)
 
         if res_bloque == "REINTENTAR":
-            time.sleep(10)  # Pausa por error de formato o cuota
+            time.sleep(10)
             continue
 
         if res_bloque:
@@ -76,8 +78,7 @@ def ejecutar():
             pd.DataFrame(resultados).to_csv(ARCHIVO_OUT, index=False, sep=';', encoding='utf-8-sig')
 
         time.sleep(1.5)
-
-
+        
 if __name__ == "__main__":
     ejecutar()
 
